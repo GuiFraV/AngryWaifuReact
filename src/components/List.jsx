c01bd8aec0e6a929b4a2f0c9a85011700d2d0a32
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-10-25 17:48:54 +0200

    2)Components : code api fetch

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,12 +1,52 @@
-import Characters from "./Characters"
+import { useEffect, useState } from "react";
+import Characters from "./Characters";
 
 function List() {
+  // Renommez isLoading en setLoading pour plus de clarté.
+  const [loading, setLoading] = useState(true);
+  const [waifu, setWaifu] = useState([]);
+
+  useEffect(() => {
+    // Définition de la fonction asynchrone getData.
+    async function getData() {
+      try {
+        const url = 'https://waifu.it/api/angry';
+        const apiToken = process.env.REACT_APP_API_TOKEN;
+        const res = await fetch(url, {
+          method: 'GET',
+          headers: {
+            Authorization: apiToken,
+          },
+        });
+
+        // Vérifiez si la réponse est ok avant de continuer.
+        if (!res.ok) {
+          throw new Error('Network response was not ok ' + res.statusText);
+        }
+
+        const data = await res.json();
+        setWaifu(data);
+        setLoading(false);  // Mettez à jour l'état de loading ici.
+      } catch (e) {
+        console.error(e.message);  // Log l'erreur au lieu de la lancer.
+        setLoading(false);  // Mettez également à jour l'état de loading en cas d'erreur.
+      }
+    }
+
+    // Appel de la fonction getData.
+    getData();
+  }, []);  // Dépendances vides signifient que cela s'exécutera une fois au montage du composant.
+
+  useEffect(() => {
+    console.log(waifu);
+  }, [waifu]);
+
   return (
     <div>
-        <p>List</p>
-        <Characters />
+      <p>List</p>
+      <Characters />
     </div>
-  )
+  );
 }
 
-export default List
+export default List;
